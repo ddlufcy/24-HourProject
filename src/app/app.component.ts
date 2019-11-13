db8688f1647896492b0b9226139b2e497cf2c562
ddlufcy <ddlufcy@gmail.com>
2019-11-13 14:09:24 -0500

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core' 
+import { People } from './people.model';
+import { PeopleDataService } from './people-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hourProject';
+export class AppComponent implements OnInit {
+  people$: People[];
+  
+  constructor(private peopleData: PeopleDataService) {} 
+
+  ngOnInit() {
+    return this.peopleData.getPeople()
+      .subscribe(data => this.people$ = data);
+  }
 }
