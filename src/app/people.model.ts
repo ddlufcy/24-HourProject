65bd1a0e36b1089e2de89ec9192515cd0799b864
alekh <alekhawkins17@gmail.com>
2019-11-13 21:48:12 -0500

    yep

--- src/app/people.model.ts
+++ src/app/people.model.ts
@@ -1,8 +1,8 @@
 export class People {
-    "id": number;
-    "eye_color": string;
-    "gender": string;
-    "hair_color": string;
-    "height": number;
-    "mass": number;
+    id: number;
+    eye_color: string;
+    gender: string;
+    hair_color: string;
+    height: number;
+    mass: number;
 }
